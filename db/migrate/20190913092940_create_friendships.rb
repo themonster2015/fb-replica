ff6d7e1fe67cb2bc477977d421386f6425e71ada
qvothi <quynhyen.vothi@gmail.com>
2021-06-15 18:07:23 +0200

    remove user-friend index to enable two rows

--- db/migrate/20190913092940_create_friendships.rb
+++ db/migrate/20190913092940_create_friendships.rb
@@ -4,11 +4,9 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true
-      t.boolean :confirmed
-
-      t.timestamps null: false
+      t.references :friend, index: true, foreign_key: { to_table: 'users' }
+      t.integer :status
     end
-    add_foreign_key :friendships, :users, column: :friend_id
-  end
+
+    end
 end
